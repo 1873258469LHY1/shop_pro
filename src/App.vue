3112c1035ee0dab202702f6f6551521fd5e77a71
LIHUAIYOU <1873258469@qq.com>
2021-01-05 09:43:00 +0800

    保留参入更改

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,25 @@
 <template>
-<<<<<<< HEAD
-
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-
-  <div class="outer">
-    <Header />
-    <CommendNav />
-    <AsideBar />
-
-    <!-- <router-view></router-view> -->
-    <Footer />
-
-    <router-view></router-view>
-    <Footer />
->>>>>>> Stashed changes
-=======
   <div class="outer">
     <!-- <Header />
     <CommendNav />
     <AsideBar /> -->
     <router-view></router-view>
     <!-- <Footer /> -->
->>>>>>> master
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import CommendNav from "./components/CommendNav";
-import AsideBar from "./components/AsideBar";
-
-=======
 // import Header from "./components/Header";
 // import Footer from "./components/Footer";
 // import CommendNav from "./components/CommendNav";
 // import AsideBar from "./components/AsideBar";
->>>>>>> master
 export default {
   name: "App",
   components: {
-<<<<<<< HEAD
-    HelloWorld,
-    Header,
-    Footer,
-    CommendNav,
-    AsideBar,
-=======
     // Header,
     // Footer,
     // CommendNav,
     // AsideBar,
->>>>>>> master
   },
 };
 </script>
@@ -64,11 +28,7 @@ export default {
 html,
 body,
 .outer {
-<<<<<<< HEAD
-  width: 100vm;
-=======
   width: 100%;
->>>>>>> master
   height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,22 +2,12 @@
 import Vue from "vue";
 import App from "./App";
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-Vue.config.productionTip = false
-=======
-=======
->>>>>>> master
 import router from "./router";
 import store from "./store";
 
 import "./plugins/element.js";
 import "./plugins/lazyload.js";
-<<<<<<< HEAD
-// import 'element-ui/lib/theme-chalk/index.css';
-=======
 import 'element-ui/lib/theme-chalk/index.css';
->>>>>>> master
 
 import "./assets/iconfont/iconfont.css"
 
@@ -26,10 +16,6 @@ import "./styles/reset.css"
 import "swiper/swiper-bundle.min.css";
 
 Vue.config.productionTip = false;
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> master
 
 new Vue({
     beforeCreate() {
