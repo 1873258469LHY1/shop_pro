c9ef92f443165bfe0c24de2ae6f64bb881c8e2e8
tphwx <825462650@qq.com>
2021-01-07 15:00:30 +0800

    feat:完成歌手歌单基本功能

--- src/api/playList.js
+++ src/api/playList.js
@@ -12,3 +12,27 @@ export const getPlayList = ({
     url: `playList?order=${order}&rn=${rn}&pn=${pn}`,
   })
 }
+
+//歌单分类Tag
+export const getPlayListTags = () => {
+  return request({
+    method: 'GET',
+    url: 'playList_tags',
+  })
+}
+
+// 获取歌单分类列表
+export const getCategoryPlayList = ({
+  tagId,
+  rn,
+  pn
+}) => {
+  return request({
+    method: 'GET',
+    url: `/playList/category?id=${tagId}`,
+    params: {
+      rn,
+      pn
+    }
+  })
+}
